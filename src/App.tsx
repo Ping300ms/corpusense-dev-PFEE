ea5173a11c14984b72e67ca312c74299a2f64092
pierre.maunier <pierre.maunier@epita.fr>
2025-06-27 18:29:15 +0200

    feat: made the app accessible even while not logged in

--- src/App.tsx
+++ src/App.tsx
@@ -7,34 +7,29 @@ import Layout from './pages/Layout'
 import ManifestExplorerPage from './pages/ManifestExplorerPage'
 import LoginPage from '@/pages/LoginPage'
 import { AuthProvider } from './contexts/AuthContext'
-import PrivateRoute from './components/PrivateRoute'
 import ProfilePage from '@/pages/ProfilePage.tsx';
 import RegisterPage from '@/pages/RegisterPage.tsx';
-import HomeRedirect from './components/HomeRedirect'
 
-const basePath: string = import.meta.env.VITE_BASE_PATH || '/'
+const basePath: string = (import.meta.env.VITE_BASE_PATH as string) || '/'
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter basename={basePath}>
         <Routes>
-          <Route index element={<HomeRedirect />} />
-
           <Route path={CorpusenseRoutes.LOGIN} element={<LoginPage />} />
           <Route path={CorpusenseRoutes.REGISTER} element={<RegisterPage />} />
 
-          <Route element={<PrivateRoute />}>
-            <Route element={<Layout />}>
-              <Route path={CorpusenseRoutes.PROFILE} element={<ProfilePage />} />
-              <Route path={CorpusenseRoutes.MANIFEST} element={<ManifestExplorerPage />} />
-              <Route path={CorpusenseRoutes.COLLECTIONS} element={<CollectionsManagerPage />} />
-              <Route
-                path={`${CorpusenseRoutes.COLLECTIONS}/:collectionId`}
-                element={<CollectionInspectorPage />}
-              />
-              <Route path={CorpusenseRoutes.CONFIGURATION} element={<ConfigurationPage />} />
-            </Route>
+          <Route element={<Layout />}>
+            <Route index element={<CollectionsManagerPage/>} />
+            <Route path={CorpusenseRoutes.PROFILE} element={<ProfilePage />} />
+            <Route path={CorpusenseRoutes.MANIFEST} element={<ManifestExplorerPage />} />
+            <Route path={CorpusenseRoutes.COLLECTIONS} element={<CollectionsManagerPage />} />
+            <Route
+              path={`${CorpusenseRoutes.COLLECTIONS}/:collectionId`}
+              element={<CollectionInspectorPage />}
+            />
+            <Route path={CorpusenseRoutes.CONFIGURATION} element={<ConfigurationPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -11,8 +11,25 @@ const ProfilePage = () => {
 
   const handleLogout = async () => {
     await supabase.auth.signOut();
-    await navigate('/'); // ou '/login' selon ta logique
+    await navigate('/profile');
   };
+
+  const handleLogin = async () => {
+    await navigate('/login');
+  }
+
+  if (!user){
+    return (
+      <div className="panel h-full flex flex-col items-center justify-center p-6">
+        <p className="text-gray-600 dark:text-gray-300">
+          {t('profile_not_logged_in')}
+        </p>
+        <Button onClick={() => void handleLogin()} className="mt-4">
+          {t('btn_login')}
+        </Button>
+      </div>
+    );
+  }
 
   return (
     <div className="panel h-full flex flex-col items-start justify-start p-6 space-y-4">
