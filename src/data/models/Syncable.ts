dfe3756d8fa33d7b32c5c6cd4ec2a0348f2b0b56
HIR021419 <90650405+HIR021419@users.noreply.github.com>
2025-10-02 18:44:08 +0200

    feat: DexieObservableProvider

--- src/data/models/Syncable.ts
+++ src/data/models/Syncable.ts
@@ -1,6 +1,13 @@
 import { WithStringId } from '@/data/models/utils.ts';
+import { Annotation } from '@/data/models/Annotation.ts';
+import { CollectionContent, CollectionDetails } from '@/data/models/Collection.ts';
+import { DataModel } from '@/data/models/DataModel.ts';
 
 export interface Syncable extends WithStringId {
   updated_at: string // Date ISO string
   synced: boolean
 }
+
+export type SyncableObject = Annotation | CollectionContent | CollectionDetails | DataModel;
+
+export const SyncableTables: string[] = ["annotations", "collections", "collectionContents", "models"];
---
+++ src/data/repositories/indexeddb/dexieObservable.ts
@@ -0,0 +1,54 @@
+import 'dexie-observable';
+import {
+  ICreateChange,
+  IDeleteChange,
+  IUpdateChange,
+} from 'dexie-observable/api';
+import { db } from '@/data/repositories/indexeddb/db.ts';
+import { SyncableObject, SyncableTables } from '@/data/models/Syncable.ts';
+
+export interface OnChangeCallbacks {
+  onAdd?: (entity: SyncableObject, table: string) => void | Promise<void>;
+  onUpdate?: (entity: SyncableObject, table: string) => void | Promise<void>;
+  onDelete?: (entity: SyncableObject, table: string) => void | Promise<void>;
+}
+
+export class DexieObservableProvider {
+  private callbacks: OnChangeCallbacks;
+
+  constructor(callbacks: OnChangeCallbacks) {
+    this.callbacks = callbacks;
+
+    db.on('changes', (changes) => {
+      for (const change of changes) {
+        if (!SyncableTables.includes(change.table)) continue;
+
+        switch (change.type as number) {
+          case 1: // CREATE
+            this.onCreate(change as ICreateChange);
+            break;
+          case 2: { // UPDATE
+            this.onUpdate(change as IUpdateChange);
+            break;
+          }
+          case 3: { // DELETE
+            this.onDelete(change as IDeleteChange);
+            break;
+          }
+        }
+      }
+    });
+  }
+
+  private onCreate(change: ICreateChange): void {
+    void this.callbacks.onAdd?.(change.obj as SyncableObject, change.table);
+  }
+
+  private onUpdate(change: IUpdateChange): void {
+    void this.callbacks.onUpdate?.(change.obj as SyncableObject, change.table);
+  }
+
+  private onDelete(change: IDeleteChange): void {
+    void this.callbacks.onDelete?.(change.oldObj as SyncableObject, change.table);
+  }
+}
