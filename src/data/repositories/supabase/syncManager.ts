04f11e77ce0f703ff4e77fc090a4cf361cc45667
Basile Sens <basile.sens@epita.fr>
2025-11-01 18:03:16 +0100

    fix(develop): changed indentation to allow debugging

--- src/data/repositories/supabase/syncManager.ts
+++ src/data/repositories/supabase/syncManager.ts
@@ -130,7 +130,8 @@ export class SyncManager {
       .select()
       .maybeSingle<Backup>();
 
-    if (error) return { data: null, error};
+    if (error)
+      return { data: null, error};
     return {data: id, error: null};
   }
 
