ceb3cfebee93186418c4a66bbabd25528a7bddd6
InsMtb <78259423+InsMtb@users.noreply.github.com>
2026-01-26 19:31:29 +0100

    feat: send mail

--- src/pages/StoragePage.tsx
+++ src/pages/StoragePage.tsx
@@ -15,6 +15,7 @@ import { useTranslation } from 'react-i18next';
 import { SyncLoader } from 'react-spinners';
 import { Loader, Lock, LockOpen, Share2 } from 'lucide-react';
 import useDialog from '@/hooks/ui/useDialog.tsx';
+import { Link } from 'react-router-dom';
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.mjs',
@@ -316,11 +317,14 @@ const StoragePage = () => {
           userManifests.map((data, index) => (
             <div
               key={index}
-              className={"mb-2 flex items-center gap-4 justify-between p-1" + (index !== userManifests.length - 1 ? ' border-b' : '')}
+              className={"mb-2 flex items-center gap-4 justify-between p-1 " + (index !== userManifests.length - 1 ? ' border-b' : '')}
             >
-              <a className="hover:text-blue-800 w-fit" href={`${hrefPath}${data.url}`}>
+              <Link
+                className="hover:text-blue-800 w-fit "
+                to={`/manifest?manifestId=${data.url}`}
+              >
                 {data.name}
-              </a>
+              </Link>
               <div className={"flex gap-2"}>
               {data.loading ? (
                 <Button>
@@ -329,14 +333,14 @@ const StoragePage = () => {
                 </Button>
               ) : data.isPrivate ? (
                 <div className="flex gap-2">
-                <Button
-                  className="text-red-500 bg-red-200 cursor-pointer"
-                  onClick={() => changeBucket(data.name, data.isPrivate)}
-                >
-                  <Lock />
-                  Privé
-                </Button>
-                  <Button className={"z-40"} onClick={() => openShareManifestDialog(userId + "/" + data.name, data.url)}>
+                  <Button
+                    className="text-red-500 bg-red-200 cursor-pointer"
+                    onClick={() => changeBucket(data.name, data.isPrivate)}
+                  >
+                    <Lock />
+                    Privé
+                  </Button>
+                  <Button className={"cursor-pointer"} onClick={() => openShareManifestDialog(userId + "/" + data.name, data.url)}>
                     <Share2 />
                     Partager
                   </Button>
